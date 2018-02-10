d521b4b958f80af830d34b92967d2df3be323a63
Akimasa Tateba <akimasa@gmail.com>
2018-02-11 00:02:06 +0900

    no need arch-chroot

--- bootstrap-common.sh
+++ bootstrap-common.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ -z "$CHROOT" ]; then CHROOT="arch-chroot"; fi
 mkdir -p /srv/arch/root/.ssh/
 chmod 700 /srv/arch/root/.ssh/
 cat sshkey.pub >> /srv/arch/root/.ssh/authorized_keys
 chmod 600 /srv/arch/root/.ssh/authorized_keys
-$chroot /srv/arch/ systemctl enable sshd
-$chroot /srv/arch/ systemctl enable avahi-daemon
-$chroot /srv/arch/ systemctl enable systemd-resolved
-$chroot /srv/arch/ ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+chroot /srv/arch/ systemctl enable sshd
+chroot /srv/arch/ systemctl enable avahi-daemon
+chroot /srv/arch/ systemctl enable systemd-resolved
+chroot /srv/arch/ ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 cp /srv/arch/usr/share/doc/avahi/ssh.service /srv/arch/etc/avahi/services/
 echo "mininger" > /srv/arch/etc/mininger
