5bae18b1f4119993ac2e7300f392ef0d5f243324
Akimasa Tateba <akimasa@gmail.com>
2018-02-10 23:53:06 +0900

    enable systemd-resolved

--- bootstrap-common.sh
+++ bootstrap-common.sh
@@ -6,5 +6,7 @@ cat sshkey.pub >> /srv/arch/root/.ssh/authorized_keys
 chmod 600 /srv/arch/root/.ssh/authorized_keys
 $chroot /srv/arch/ systemctl enable sshd
 $chroot /srv/arch/ systemctl enable avahi-daemon
+$chroot /srv/arch/ systemctl enable systemd-resolved
+$chroot /srv/arch/ ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 cp /srv/arch/usr/share/doc/avahi/ssh.service /srv/arch/etc/avahi/services/
 echo "mininger" > /srv/arch/etc/mininger
