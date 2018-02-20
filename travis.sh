2fdc42b8ca28ba7615eb2e4763a146dc42b1f330
Akimasa Tateba <akimasa@gmail.com>
2018-02-20 21:02:10 +0900

    shrink!

--- travis.sh
+++ travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-pacman -Syu --noconfirm
+#pacman -Syu --noconfirm
 pacman --noconfirm -S squashfs-tools
 mkdir /run/shm
 cd /root
 bash bootstrap-arch.sh
 bash setip.sh
-mksquashfs /srv/arch/ /root/arch.sfs -comp xz -Xbcj x86 -e /srv/arch/boot/
+mksquashfs /srv/arch/ /root/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
 ls -lh /root/arch.sfs
