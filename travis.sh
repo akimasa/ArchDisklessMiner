695b4bb584ac76df273f2693ba83d8eea9c11490
Akimasa Tateba <akimasa@gmail.com>
2018-02-21 00:19:30 +0900

    test!

--- travis.sh
+++ travis.sh
@@ -5,4 +5,4 @@ mkdir /run/shm
 cd /root
 bash bootstrap-arch.sh
 bash setip.sh
-mksquashfs /srv/arch/ /srv/http/arch.sfs -e /srv/arch/boot/
+mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
