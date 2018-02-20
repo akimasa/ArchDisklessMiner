796e31294e7d6083ec75095606afe41cdd778a53
Akimasa Tateba <akimasa@gmail.com>
2018-02-21 00:48:15 +0900

    add .travis.yml

---
+++ travis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pacman -Syu --noconfirm
+pacman --noconfirm -S squashfs-tools
+mkdir /run/shm
+cd /root
+bash bootstrap-arch.sh
+bash setip.sh
+mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
