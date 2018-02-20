cadfb9e4bcfabceba7486cc99b135c281ea4952f
Akimasa Tateba <akimasa@gmail.com>
2018-02-20 22:04:29 +0900

    fix

--- travis.sh
+++ travis.sh
@@ -5,5 +5,4 @@ mkdir /run/shm
 cd /root
 bash bootstrap-arch.sh
 bash setip.sh
-mksquashfs /srv/arch/ /root/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
-ls -lh /root/arch.sfs
+mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
