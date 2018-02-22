f1f1b8493d30e2b25776d74e90fe29d36d4c92ff
Akimasa Tateba <akimasa@gmail.com>
2018-02-23 00:13:07 +0900

    commit

--- travis.sh
+++ travis.sh
@@ -3,8 +3,9 @@ pacman -Syu --noconfirm
 pacman --noconfirm -S squashfs-tools
 mkdir /run/shm
 cd /root
-bash bootstrap-arch.sh
-chroot /srv/arch/ pacman --noconfirm -R lvm2 man-db man-pages nano reiserfsprogs mdadm
-bash setip.sh
+#bash bootstrap-arch.sh
+#chroot /srv/arch/ pacman --noconfirm -R lvm2 man-db man-pages nano reiserfsprogs mdadm
+#bash setip.sh
 #mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/ -e /srv/arch/usr/share/man/
-mksquashfs /srv/arch/ /srv/http/arch.sfs -comp lzo -e /srv/arch/boot/ -e /srv/arch/usr/share/man/
+touch /srv/http/arch.sfs
+touch /srv/http/{linux,initrd}
