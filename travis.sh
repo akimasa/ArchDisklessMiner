e0e8ac14cdf80cad4dfff8e47c48f74fbd4004a7
Akimasa Tateba <akimasa@gmail.com>
2018-02-22 22:51:03 +0900

    squashfs comp: lzo checkout master

--- travis.sh
+++ travis.sh
@@ -6,4 +6,5 @@ cd /root
 bash bootstrap-arch.sh
 chroot /srv/arch/ pacman --noconfirm -R lvm2 man-db man-pages nano reiserfsprogs mdadm
 bash setip.sh
-mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/ -e /srv/arch/usr/share/man/
+#mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/ -e /srv/arch/usr/share/man/
+mksquashfs /srv/arch/ /srv/http/arch.sfs -comp lzo -e /srv/arch/boot/ -e /srv/arch/usr/share/man/
