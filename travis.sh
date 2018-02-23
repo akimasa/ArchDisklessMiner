1f746f4b9d9983bda05950788d6a0646832eff63
Akimasa Tateba <akimasa@gmail.com>
2018-02-24 00:24:39 +0900

    test travis_fold

--- travis.sh
+++ travis.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#pacman -Syu --noconfirm
-#pacman --noconfirm -S squashfs-tools
-#mkdir /run/shm
-#cd /root
-#bash bootstrap-arch.sh
-#chroot /srv/arch/ pacman --noconfirm -R lvm2 man-db man-pages nano reiserfsprogs mdadm
-#bash setip.sh
-#mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/ -e /srv/arch/usr/share/man/
-echo 1 > /srv/http/arch.sfs
-echo 1 > /srv/http/linux
-echo 1 > /srv/http/initrd
+pacman -Syu --noconfirm
+pacman --noconfirm -S squashfs-tools
+mkdir /run/shm
+cd /root
+echo travis_fold:start:bootstrap
+bash bootstrap-arch.sh
+echo travis_fold:end:bootstrap
+echo travis_fold:start:mkinitcpio
+bash setip.sh
+echo travis_fold:end:mkinitcpio
+echo travis_fold:start:mksquashfs
+mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
+echo travis_fold:end:mksquashfs
