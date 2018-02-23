17b6d88c49c4190f56277b8e6906502ed137ea87
Akimasa Tateba <akimasa@gmail.com>
2018-02-24 00:31:45 +0900

    fixup

--- travis.sh
+++ travis.sh
@@ -3,12 +3,12 @@ pacman -Syu --noconfirm
 pacman --noconfirm -S squashfs-tools
 mkdir /run/shm
 cd /root
-echo travis_fold:start:bootstrap
+echo -e "travis_fold:start:bootstrap\r"
 bash bootstrap-arch.sh
-echo travis_fold:end:bootstrap
-echo travis_fold:start:mkinitcpio
+echo -e "travis_fold:end:bootstrap\r"
+echo -e "travis_fold:start:mkinitcpio\r"
 bash setip.sh
-echo travis_fold:end:mkinitcpio
-echo travis_fold:start:mksquashfs
+echo -e "travis_fold:end:mkinitcpio\r"
+echo -e "travis_fold:start:mksquashfs\r"
 mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
-echo travis_fold:end:mksquashfs
+echo -e "travis_fold:end:mksquashfs\r"
