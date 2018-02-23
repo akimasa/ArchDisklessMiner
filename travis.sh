8a0871c58bdff3b3fe715e8682aed81a0a0133a1
Akimasa Tateba <akimasa@gmail.com>
2018-02-24 00:24:39 +0900

    travis_fold

--- travis.sh
+++ travis.sh
@@ -3,6 +3,12 @@ pacman -Syu --noconfirm
 pacman --noconfirm -S squashfs-tools
 mkdir /run/shm
 cd /root
+echo travis_fold:start:bootstrap
 bash bootstrap-arch.sh
+echo travis_fold:end:bootstrap
+echo travis_fold:start:mkinitcpio
 bash setip.sh
+echo travis_fold:end:mkinitcpio
+echo travis_fold:start:mksquashfs
 mksquashfs /srv/arch/ /srv/http/arch.sfs -comp xz -Xbcj x86 -b 1M -e /srv/arch/boot/
+echo travis_fold:end:mksquashfs
